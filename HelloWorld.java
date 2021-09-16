7a1246e49efa1e3e1b1f3bb2bc2851c53e2735e3
Лифанов Валерий Павлович <v.lifanov@univ.priv>
2021-09-16 09:37:08 +0300

    solving merge conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World1!");
+<<<<<<< HEAD
+=======
+		System.out.println("Hello World!1"+ "\n");
+>>>>>>> fdaafc6 (added \n in output)
 }
 }
